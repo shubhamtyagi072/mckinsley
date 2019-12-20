ce58c2ab53b853f67c8fce83c917113e3158fd2f
shubhamtyagi072 <37326465+shubhamtyagi072@users.noreply.github.com>
2019-12-20 15:36:53 +0530

    Add files via upload

---
+++ BoilerPlate1/BoilerPlate1/Controller/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  BoilerPlate1
+//
+//  Created by shubham tyagi on 14/12/19.
+//  Copyright © 2019 shubham tyagi. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red;
+    }
+}
